384a8c0bbe1b96727e7eefcce8c759ab22123d61
Bradley Bartholomew <bradley.bartholomew@live.com>
2015-06-06 11:33:09 -0700

    Add test code

---
+++ test/project/app/scripts/fake.component.js
@@ -0,0 +1,9 @@
+(function(module) {
+    module.test = {
+        // THIS IS JUST TEST CODE TO MAKE SURE THINGS GO WELL
+        fakeFunc: function() {
+            return 3;
+        },
+        fakeValue: 'fake'
+    };
+}(window.module || {}));
